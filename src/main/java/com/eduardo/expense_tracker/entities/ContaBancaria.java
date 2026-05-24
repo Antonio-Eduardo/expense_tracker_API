23ff7e5d0dcaf536dd80adc765674dc6444e8a77
Antonio Eduardo Moreira Oliveira <119017563+Antonio-Eduardo@users.noreply.github.com>
2026-05-23 23:55:20 -0300

    feat: ContaBancaria

--- src/main/java/com/eduardo/expense_tracker/entities/ContaBancaria.java
+++ src/main/java/com/eduardo/expense_tracker/entities/ContaBancaria.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Set;
+
 @Entity
 @Table(name = "tb_conta_bancaria")
 @Data
@@ -15,4 +19,15 @@ public class ContaBancaria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private BigDecimal balance;
+    private String tipoConta;
+    private Instant creditCardClosingDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "contaBancaria", cascade = CascadeType.ALL)
+    private Set<GastoMensal> gastosMensais;
 }
