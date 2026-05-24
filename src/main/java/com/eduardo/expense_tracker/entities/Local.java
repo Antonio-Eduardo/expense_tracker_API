d2cee3c9d6ce2fd5d71c8582cb8b57dfcab1bd86
Antonio Eduardo Moreira Oliveira <119017563+Antonio-Eduardo@users.noreply.github.com>
2026-05-23 23:40:57 -0300

    feat: Local

--- src/main/java/com/eduardo/expense_tracker/entities/Local.java
+++ src/main/java/com/eduardo/expense_tracker/entities/Local.java
@@ -1,4 +1,24 @@
 package com.eduardo.expense_tracker.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_local")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Local {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String city;
+    private String state;
+    private String addres1;
+    private String addres2;
+    private String zipCode;
 }
