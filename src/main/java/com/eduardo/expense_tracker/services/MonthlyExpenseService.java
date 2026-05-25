f56a1a455be3730c83ec91dd1507084a41d0c7ac
Antonio Eduardo Moreira Oliveira <119017563+Antonio-Eduardo@users.noreply.github.com>
2026-05-25 13:47:36 -0300

    feat: MonthlyExpenseService

--- src/main/java/com/eduardo/expense_tracker/services/MonthlyExpenseService.java
+++ src/main/java/com/eduardo/expense_tracker/services/MonthlyExpenseService.java
@@ -1,4 +1,34 @@
 package com.eduardo.expense_tracker.services;
 
+import com.eduardo.expense_tracker.entities.MonthlyExpense;
+import com.eduardo.expense_tracker.repositories.MonthlyExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MonthlyExpenseService {
+
+    @Autowired
+    MonthlyExpenseRepository repository;
+
+    public MonthlyExpense insertMonthlyExpense(MonthlyExpense monthlyExpense){
+        return repository.save(monthlyExpense);
+    }
+    public MonthlyExpense findMonthlyExpenseById(Long id){
+        return repository.findById(id).orElse(null);
+    }
+    public void deleteMonthlyExpense(Long id) {
+        repository.deleteById(id);
+    }
+        public MonthlyExpense updateMonthlyExpense(Long id, MonthlyExpense obj){
+            MonthlyExpense monthlyExpenseFind = repository.findById(id).orElse(null);
+            if (monthlyExpenseFind != null) {
+                updateData(monthlyExpenseFind, obj);
+                return repository.save(monthlyExpenseFind);
+            }
+            return null;
+        }
+        private void updateData(MonthlyExpense monthlyExpenseFind, MonthlyExpense obj) {
+            monthlyExpenseFind.setLimitExpense(obj.getLimitExpense());
+        }
 }
